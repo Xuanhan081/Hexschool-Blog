b5e386f2a873049c818729d42c4bf97d7df54780
Xuanhan081 <melodymelody0801@gmail.com>
2025-05-28 23:35:34 +0800

    加AOS動畫

--- JS/all.js
+++ JS/all.js
@@ -1,5 +1,5 @@
 AOS.init({
-        offset: 200, // offset (in px) from the original trigger point
+        offset: 100, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 2000 // values from 0 to 3000, with step 50ms
       })
