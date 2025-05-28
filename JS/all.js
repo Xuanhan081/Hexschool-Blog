d21d3003b23ccbdd039eba1923602913b4ef6464
Xuanhan081 <melodymelody0801@gmail.com>
2025-05-28 23:26:34 +0800

    修改css

--- JS/all.js
+++ JS/all.js
@@ -1,5 +1,5 @@
 AOS.init({
-        offset: 450, // offset (in px) from the original trigger point
+        offset: 200, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 2000 // values from 0 to 3000, with step 50ms
       })
@@ -20,7 +20,6 @@ function slideLeft() {
 }
 
 function slideRight() {
-    debugger; // Add this line
     if (currentIndex < cards.length - cardsPerView) {
         currentIndex++;
         updateSlider();
